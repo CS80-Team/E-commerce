ed16946882f17201f01a3f9b5d3f8ee672077a9f
Omar Muhammad <omarmgaber37@gmail.com>
2024-05-16 03:36:41 +0300

    Resolvinf conflicts

--- src/main/java/swe/project/ecommerce/service/impl/ProdDetailsService.java
+++ src/main/java/swe/project/ecommerce/service/impl/ProdDetailsService.java
@@ -1,4 +1,4 @@
-package swe.project.ecommerce.service.impl;
+package swe.project.ecommerce.service.Impl;
 
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
