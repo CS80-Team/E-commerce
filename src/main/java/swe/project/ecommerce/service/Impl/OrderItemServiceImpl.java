931d080c412768abeda2237ea169d3332267e8f2
Omar Muhammad <66267372+OmarMGaber@users.noreply.github.com>
2024-05-16 03:42:36 +0300

    Delete src/main/java/swe/project/ecommerce/service/Impl directory

--- src/main/java/swe/project/ecommerce/service/Impl/OrderItemServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package swe.project.ecommerce.service.Impl;
-
-import swe.project.ecommerce.repository.OrderItemRepository;
-import swe.project.ecommerce.service.OrderItemService;
-
-public class OrderItemServiceImpl implements OrderItemService {
-    private OrderItemRepository orderItemRepository;
-}
