cd0fbe549963678dff1e09f831a8d1ebb6196e90
Omar Muhammad <omarmgaber37@gmail.com>
2024-05-16 03:58:26 +0300

    Repo Annotation

--- src/main/java/swe/project/ecommerce/repository/DiscountRepository.java
+++ src/main/java/swe/project/ecommerce/repository/DiscountRepository.java
@@ -1,10 +1,12 @@
 package swe.project.ecommerce.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import swe.project.ecommerce.model.Discount;
 
 import java.util.UUID;
 
+@Repository
 public interface DiscountRepository extends JpaRepository<Discount, UUID> {
     Discount findByProductId(UUID productId);
 }
